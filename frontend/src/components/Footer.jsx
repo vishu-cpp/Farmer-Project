ea664119ba3fca0f061c4018df528582bd0f02b7
Vishesh <vishu12113@gmail.com>
2024-07-29 01:56:43 +0530

    complete

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -25,8 +25,8 @@ function Footer() {
             </p>
         </aside>
         <nav>
-            <h6 className="footer-title">Social</h6>
-            <div className="grid grid-flow-col gap-4">
+            {/* <h6 className="footer-title">Social</h6> */}
+            {/* <div className="grid grid-flow-col gap-4">
             <a>
                 <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -47,6 +47,7 @@ function Footer() {
                 className="fill-current">
                 <path
                     d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                <a href=""></a>
                 </svg>
             </a>
             <a>
@@ -60,7 +61,7 @@ function Footer() {
                     d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                 </svg>
             </a>
-            </div>
+            </div> */}
         </nav>
         </footer>
     

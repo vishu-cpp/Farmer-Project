b398f923f6e0e6792b0ba3b482fe3321140b389f
Vishesh <vishu12113@gmail.com>
2025-01-31 18:18:09 +0530

    fixeed code

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,29 +1,15 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+    },
+    darkMode: "class",
+    plugins: [
+      require('daisyui'),
+    ],
 
-// export default {
-//     content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
-//     theme: {
-//       extend: {},
-//     },
-//     darkMode: "class",
-//     plugins: [
-//       require('daisyui'),
-//     ],
-
-// };
-
-
-// tailwind.config.js
-const daisyui = require('daisyui');
-
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [
-    daisyui,
-  ],
 };
+
+
 

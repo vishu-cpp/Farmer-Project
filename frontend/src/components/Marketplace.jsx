677bf922ca0fc7a01ef838a0bc9dbb1544e66c85
Vishesh <vishu12113@gmail.com>
2025-01-31 21:32:23 +0530

    marketplacecode change

--- frontend/src/components/Marketplace.jsx
+++ frontend/src/components/Marketplace.jsx
@@ -1,51 +1,102 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-import Cards from "./Cards" 
+// import Cards from "./Cards" 
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import book from "../../../backend/model/book.model.js"
+// function Marketplace() {
+//   const [book,setBook]=useState([])
+//   //to get the data from the backend
+//   useEffect(() => {
+//     const getBook = async()=>{
+//       try{
+//           const res = await axios.get("https://farmer-project.onrender.com/book");
+//           console.log(res.data);
+//           setBook(res.data);
+//       } 
+//       catch(error){
+//         console.log("error")
+
+//       }
+//     }
+//     getBook();
+//   },[])
+
+//   return (
+//     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+//       <div className="mt-20 item-center text-center justify-center">
+//         <h1 className="text-2xl md:text-4xl p-3">
+//           Welcome to
+//           <span className="text-green-500"> MarketPlace</span>
+//         </h1>
+//         <p className="mt-3">
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+//           error, aspernatur, molestias, labore eaque quo veniam reiciendis
+//           dolorum debitis repellat qui optio iure minima veritatis quis dicta a
+//           magni commodi!
+//         </p>
+//         <Link to="/">
+//           <button className="mt-6 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-emerald-700 duration-300">Back</button>
+//         </Link>
+//       </div>
+//       <div className="mt-3 grid grid-cols-1 md:grid-cols-4">
+//         {
+//           book.map((item) =>(
+//             <Cards key={item.id} item={item} />
+//           ))
+
+//         }
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Marketplace;
+
+
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import book from "../../../backend/model/book.model.js"
-function Marketplace() {
-  const [book,setBook]=useState([])
-  //to get the data from the backend
-  useEffect(() => {
-    const getBook = async()=>{
-      try{
-          const res = await axios.get("https://farmer-project.onrender.com/book");
-          console.log(res.data);
-          setBook(res.data);
-      } 
-      catch(error){
-        console.log("error")
 
+function Marketplace() {
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("https://farmer-project.onrender.com/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.error("Error fetching books:", error);
       }
-    }
+    };
+
     getBook();
-  },[])
+  }, []);
 
   return (
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-      <div className="mt-20 item-center text-center justify-center">
+      <div className="mt-20 text-center">
         <h1 className="text-2xl md:text-4xl p-3">
-          Welcome to
-          <span className="text-green-500"> MarketPlace</span>
+          Welcome to <span className="text-green-500">MarketPlace</span>
         </h1>
         <p className="mt-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-          error, aspernatur, molestias, labore eaque quo veniam reiciendis
-          dolorum debitis repellat qui optio iure minima veritatis quis dicta a
-          magni commodi!
+          Explore a variety of books available for you!
         </p>
         <Link to="/">
-          <button className="mt-6 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-emerald-700 duration-300">Back</button>
+          <button className="mt-6 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-emerald-700 duration-300">
+            Back
+          </button>
         </Link>
       </div>
-      <div className="mt-3 grid grid-cols-1 md:grid-cols-4">
-        {
-          book.map((item) =>(
-            <Cards key={item.id} item={item} />
-          ))
-
-        }
+      <div className="mt-3 grid grid-cols-1 md:grid-cols-4 gap-4">
+        {book.length > 0 ? (
+          book.map((item) => <Cards key={item._id || item.id} item={item} />)
+        ) : (
+          <p className="text-center col-span-4 mt-6">No books available.</p>
+        )}
       </div>
     </div>
   );

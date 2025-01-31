e2cc4e5ca81ba996d5437783e31eb537091e87ee
Vishesh <vishu12113@gmail.com>
2025-01-31 17:47:43 +0530

    tailwindfix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,29 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
-    },
-    darkMode: "class",
-    plugins: [
-      require('daisyui'),
-    ],
+// /** @type {import('tailwindcss').Config} */
 
+// export default {
+//     content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+//     theme: {
+//       extend: {},
+//     },
+//     darkMode: "class",
+//     plugins: [
+//       require('daisyui'),
+//     ],
+
+// };
+
+
+// tailwind.config.js
+const daisyui = require('daisyui');
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    daisyui,
+  ],
 };
-
-
 
